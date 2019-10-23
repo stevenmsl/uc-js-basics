d8d5110c0e68e19aa3be903357fa9a5c641995e5
Steven Lin <steven_lx@outlook.com>
2019-10-23 06:11:12 -0700

    Rest Parameters and Arguments

--- src/index.js
+++ src/index.js
@@ -13,7 +13,25 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 //Function Return Values
 
+/*
 //Rest Parameters and Arguments
+function makeCarPrice() { //old way
+    console.log(arguments, Array.isArray(arguments)); //false, arguments is not an array
+    const total = Array.from(arguments)
+        .reduce((prev, next) => {
+            return prev + next;
+        }); 
+    console.log(`Total: ${total}USD`);
+}
+makeCarPrice(11, 44, 55, 99, 66);
+
+function makeCarPriceRest(...params) {
+    console.log(Array.isArray(params));
+    const total = params.reduce((prev, next) => prev + next);
+    console.log(`Total: ${total}USD`);
+}
+makeCarPriceRest(99, 88, 77, 11, 44);
+*/
 
 /*
 //Function Parameters and Defaults
