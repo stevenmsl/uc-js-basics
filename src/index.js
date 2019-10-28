c1b389bcd8a82a8a071567f81d7037c63ff2b3e0
Steven Lin <steven_lx@outlook.com>
2019-10-28 06:50:32 -0700

    Imperative Array Iteration

--- src/index.js
+++ src/index.js
@@ -16,6 +16,14 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 //Iteration with Break and Continue
 
 //Imperative Array Iteration
+const drinks = ['Pepsi', 'Lemonade', 'Cola'];
+
+for (let i = 0; i < drinks.length; i++) {
+    const drink = drinks[i];
+    if (drink === 'Lemonade') {
+        console.log('Pour me a class!');
+    }    
+}
 
 /*
 //Correctly Type-Checking Arrays
